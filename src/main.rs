8308dcfa9325a9fcf85ec14b7954a25320a80ebb
Ryan Ernst <rernst@iq-inc.com>
2018-12-01 19:44:37 -0500

    Cleaning up structure.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use std::fs::File;
-use std::io::prelude::*;
 
 mod slns;
 
 fn main() {
-
-    let mut file = File::open("ProblemInput/Day1Part1.txt").expect("Failed to open file!");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Failed to parse file contents");
-
-    let split = contents.split("\n");
-
-    let vec = split.map(|x| x.trim()).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-
-    let answer = slns::d1_p1::frequency_drift(&vec);
-
-    println!("The first answer is: {}", answer);
-
-    let answer = slns::d1_p2::frequency_calibrate(&vec);
-
-    println!("The second answer is: {}", answer);
+    //slns::solve_day_1();
+    slns::solve_day_2();
 }
--- src/slns/mod.rs
+++ src/slns/mod.rs
@@ -1,2 +1,25 @@
+pub mod utils;
+
 pub mod d1_p1;
 pub mod d1_p2;
+
+pub fn solve_day_1() {
+    let contents = utils::get_problem_input("ProblemInput/Day1.txt");
+
+    let split = contents.split("\n");
+
+    let vec = split.map(|x| x.trim()).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+
+    let answer = d1_p1::frequency_drift(&vec);
+
+    println!("The first answer is: {}", answer);
+
+    let answer = d1_p2::frequency_calibrate(&vec);
+
+    println!("The second answer is: {}", answer);
+}
+
+pub fn solve_day_2() {
+    let contents = utils::get_problem_input("ProblemInput/Day2.txt");
+
+}
